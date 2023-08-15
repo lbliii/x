c3a5e5eef562d37949dd7cf3004805de3593a987
L.B <mrlawrencelane@gmail.com>
2023-08-15 14:00:32 -0400

    full path to determined

--- source/conf.py
+++ source/conf.py
@@ -16,7 +16,7 @@ import os, sys
 if sys.platform == 'darwin':
     sys.path.insert(0, '/Users/*/Documents/github/determined')
 
-sys.path.insert(0, '../../determined')
+sys.path.insert(0, '/home/runner/work/x/x/determined')
 
 # -- Project information -----------------------------------------------------
 
